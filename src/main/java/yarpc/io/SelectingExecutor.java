a7d4703312cddcd94a69578d49d1b5b2a7bc5b75
Jay Booth <jay@jay-laptop.(none)>
2010-10-05 15:34:48 -0400

    timeout suppport

--- src/main/java/yarpc/io/SelectingExecutor.java
+++ src/main/java/yarpc/io/SelectingExecutor.java
@@ -5,6 +5,9 @@ import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.NavigableMap;
 import java.util.Set;
 import java.util.TreeMap;
@@ -12,13 +15,15 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 import java.util.concurrent.RunnableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.google.common.base.Supplier;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import com.google.common.util.concurrent.AbstractFuture;
 
 
@@ -67,10 +72,10 @@ public class SelectingExecutor {
   }
   
   public <V> Future<V> submit(SelectableChannel selectable, int interestOps, int timeout, TimeoutCallable<V> toCall) {
-    FutureTask<V> ret = new FutureTask<V>(toCall);
-    SelectingFuture todo = new SelectingFuture(selectable, interestOps, timeout, ret);
+    
+    SelectingFuture todo = new SelectingFuture(selectable, interestOps, timeout, toCall);
     selectThreads[roundRobin()].submit(todo);
-    return ret;
+    return todo;
   }
   
   public int roundRobin() {
@@ -89,28 +94,51 @@ public class SelectingExecutor {
   }
 
   /* Composite used to pass information about an operation to a SelectThread */
-  private static class SelectingFuture<T> extends AbstractFuture {
+  private static class SelectingFuture<T> extends AbstractFuture<T> implements RunnableFuture<T> {
     final SelectableChannel selectable;
     final int interestOps;
-    final TimeoutCallable toRun;
+    final TimeoutCallable<T> toRun;
     final long timeoutTime;
     
-    public SelectingFuture(SelectableChannel selectable, int interestOps, int timeout, TimeoutCallable toRun) {
+    public SelectingFuture(SelectableChannel selectable, int interestOps, int timeout, TimeoutCallable<T> toRun) {
       this.selectable=selectable;
       this.interestOps=interestOps;
       this.toRun=toRun;
-      if (timeout == -1) this.timeoutTime = -1;
+      if (timeout <= 0) this.timeoutTime = -1;
       else this.timeoutTime = System.currentTimeMillis() + timeout;
     }
-
     
+    // call this method on normal dispatch
+    @Override
+    public void run() {
+      try {
+        set(toRun.call());
+      } catch (Throwable t) {
+        setException(t);
+      }
+    }
+    
+    // call this method if we timed out before running
+    public void timeout() {
+      try {
+    	  set(toRun.onTimeout());
+      } catch (Throwable t) {
+    	 setException(t);
+      }
+    }
   }
   
   private class SelectThread extends Thread {
     private volatile boolean running;
     private final Selector select;
     private final ConcurrentLinkedQueue<SelectingFuture> inbox = new ConcurrentLinkedQueue<SelectingFuture>();
-    private final NavigableMap<Long,SelectingFuture> timeouts = new TreeMap<Long,SelectingFuture>();
+    private final NavigableMap<Long,Collection<SelectingFuture>> timeoutKeys = new TreeMap<Long,Collection<SelectingFuture>>();
+    private final Multimap<Long, SelectingFuture> timeoutsMulti = Multimaps.newListMultimap(timeoutKeys, new Supplier<List<SelectingFuture>>() {
+      @Override
+      public List<SelectingFuture> get() {
+        return new ArrayList<SelectingFuture>();
+      }
+    });
     
     public SelectThread() throws IOException {
       this.select=Selector.open();
@@ -123,14 +151,17 @@ public class SelectingExecutor {
     @Override
     public void run() {
       running = true;
+      // main loop has 4 phases
+      // 1)  
+      
       while (running) {
-        // scan inbox, register them
+        // 1) scan inbox, register them
         SelectingFuture s;
         while ((s = inbox.poll()) != null) {
           try {
-            // register timeout
+            // register timeout in multimap
             if (s.timeoutTime > 0) {
-              timeouts.put(s.timeoutTime, s);
+              timeoutsMulti.put(s.timeoutTime, s);
             }
             // register for select
             s.selectable.register(select, s.interestOps, s.toRun);
@@ -138,8 +169,9 @@ public class SelectingExecutor {
             LOG.warn("Exception registering task! " + s + " : " + e);
           }
         }
+        // 2) select
         // timeout for our select either the time until the shortest timeout is up, or 1ms
-        long shortestTimeout =timeouts.firstKey() - System.currentTimeMillis();
+        long shortestTimeout =timeoutKeys.firstKey() - System.currentTimeMillis();
         try {
           // if shortestTimeout is in the past, just selectNow
           if (shortestTimeout > 0) select.select(shortestTimeout);
@@ -147,6 +179,7 @@ public class SelectingExecutor {
         } catch (IOException ioe) {
           LOG.fatal("Exception selecting!  Dying.. : " + Utils.stringifyException(ioe));
         }
+        // 3) dispatch
         // dispatch active selectionkeys to the executor service
         Set<SelectionKey> selected = select.selectedKeys();
         for (SelectionKey key : selected) {
@@ -156,18 +189,31 @@ public class SelectingExecutor {
           }
           // interest set 0 so we don't launch it again
           key.interestOps(0);
-          // take it out of the timeout tree because we don't care anymore
           
-          
-          // launch
           SelectingFuture task = (SelectingFuture) key.attachment();
+          // take it out of the timeout tree because we don't care anymore
+          timeoutsMulti.remove(task.timeoutTime, task);
+          // launch
           exec.submit(task.toRun);
         }
         // clear selected to avoid busyloops
         selected.clear();
+        // 4) timeout
         // now prune all expired tasks
-        
-        
+        Set<Long> timedOutKeys = timeoutKeys.headMap(System.currentTimeMillis(), true).keySet();
+        for (long timedOut : timedOutKeys) {
+          // remove all Futures that timeout at this millis
+          Collection<SelectingFuture> futuresWithKey = timeoutsMulti.removeAll(timedOut);
+          // submit timeout return
+          for (final SelectingFuture f : futuresWithKey) {
+            exec.submit(new Runnable() {
+              @Override
+              public void run() {
+                f.timeout();
+              }
+            });
+          }
+        }
       }
     }
     
